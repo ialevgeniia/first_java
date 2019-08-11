ea0e7bedff2cfa888d33a8122ebe82263b4e0045
Evgeniia Ialymova <eugeniyayal@gmail.com>
2019-08-11 18:14:31 +0300

    second

---
+++ sandbox/src/main/java/ru/stqa/pft/second/start.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.second;
+
+public class start {
+
+    public static void main (String[] args){ ;
+        Point p = new Point (2,5,7,8);
+        System.out.println("Расстояние между точкми = " + p.distance());
+    }
+
+}
