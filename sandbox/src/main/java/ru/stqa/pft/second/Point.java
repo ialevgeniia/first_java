f631f329fb523866751639b72771621ec32512f5
Evgeniia Ialymova <eugeniyayal@gmail.com>
2019-08-18 17:04:51 +0300

    2

--- sandbox/src/main/java/ru/stqa/pft/second/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/second/Point.java
@@ -1,20 +1,22 @@
 package ru.stqa.pft.second;
 
 public class Point {
-    double x1;
-    double y1;
-    double x2;
-    double y2;
+    double x;
+    double y;
 
-    public Point (double x1, double y1, double x2, double y2)
+    Point (double x,double y)
     {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+        this.x = x;
+        this.y=y;
     }
 
-    public double distance(){
-            return Math.sqrt((this.x2-this.x1)*(this.x2-this.x1)+(this.y2-this.y1)*(this.y2-this.y1));
+    public double distance(Point p) {
+        return distance(p.x, p.y);
+    }
+
+    public double distance(double x, double y){
+        return Math.sqrt((this.x-x)*(this.x-x)+(this.y-y)*(this.y-y));
         }
+
+
 }
--- sandbox/src/main/java/ru/stqa/pft/second/start.java
+++ sandbox/src/main/java/ru/stqa/pft/second/start.java
@@ -3,8 +3,10 @@ package ru.stqa.pft.second;
 public class start {
 
     public static void main (String[] args){ ;
-        Point p = new Point (2,5,7,8);
-        System.out.println("Расстояние между точкми = " + p.distance());
+        Point p1 = new Point (5,1);
+        Point p2 = new Point (8,9);
+        System.out.println("Расстояние между точкaми = " + p1.distance(p2));
     }
+
 
 }
